35cfae608f90d937ca1b8225d91857d9e7b9110c
manikachaudhary <cmanika23@gmail.com>
2025-06-15 10:16:43 -0500

    Initial springboot scaffold with working Maven build

---
+++ user-service/src/main/java/com/pulsetrade/user_service/PulsetradeUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pulsetrade.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsetradeUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsetradeUserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/pulsetrade/user_service/PulsetradeUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsetrade.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsetradeUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
