957005204e1e6ac54c8a6ac24af2338dcfac70f1
manikachaudhary <cmanika23@gmail.com>
2025-06-15 15:14:32 -0500

    Authentication controller

---
+++ user-service/src/main/java/com/pulsetrade/user_service/api/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.pulsetrade.user_service.api.controller;
+
+import com.pulsetrade.user_service.api.dto.AuthResponse;
+import com.pulsetrade.user_service.api.dto.LoginRequest;
+import com.pulsetrade.user_service.api.dto.RegisterRequest;
+import com.pulsetrade.user_service.application.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @Autowired
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+
+}
