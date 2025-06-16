871728cdb936b3c6588e87637ec08030a0dfce31
manikachaudhary <cmanika23@gmail.com>
2025-06-16 09:24:56 -0500

    entity for user profile, repository and flyway script

---
+++ user-service/src/main/java/com/pulsetrade/user_service/domain/model/UserProfile.java
@@ -0,0 +1,35 @@
+package com.pulsetrade.user_service.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_profiles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserProfile {
+
+    @Id
+    private UUID id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "bio")
+    private String bio;
+
+
+}
---
+++ user-service/src/main/java/com/pulsetrade/user_service/infrastructure/repository/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.pulsetrade.user_service.infrastructure.repository;
+
+import com.pulsetrade.user_service.domain.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, UUID> {
+    Optional<UserProfile> findByUserId(UUID userId);
+}
